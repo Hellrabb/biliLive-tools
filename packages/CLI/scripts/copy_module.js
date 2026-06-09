f7d1050cee6ef6524c0942a12b548f80f76d54bb
Hellrabb <lz18234002278@gmail.com>
2026-06-10 01:25:26 +0800

    fix(CLI): 更新 copy_module 脚本

--- packages/CLI/scripts/copy_module.js
+++ packages/CLI/scripts/copy_module.js
@@ -43,13 +43,9 @@ function main() {
     },
   );
   // 复制meyda相关文件，
-  fs.cpSync(
-    path.join(pnpm_node_modules, "meyda"),
-    path.join(cli_node_modules, "meyda"),
-    {
-      recursive: true,
-    },
-  );
+  fs.cpSync(path.join(pnpm_node_modules, "meyda"), path.join(cli_node_modules, "meyda"), {
+    recursive: true,
+  });
   // 复制shazamio-core相关文件，
   fs.cpSync(
     path.join(pnpm_node_modules, "shazamio-core"),
